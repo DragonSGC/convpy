47ceedecd03333010063444a1ceda655c82f4fc3
DragonSGC <simongc100@gmail.com>
2024-05-20 17:04:18 +0200

    Linting and formatting, added pre-commit

--- pyconv/pyconv.py
+++ pyconv/pyconv.py
@@ -2,11 +2,11 @@
 Filename: pyconv.py
 Author: Simon Crampton
 Created: May 16, 2024
-Description: This module functions along with click setup to quickly convert 
+Description: This module functions along with click setup to quickly convert
             numbers to different base types
 
 License:
-    This code is provided under the GPL V3 License. See the LICENSE file 
+    This code is provided under the GPL V3 License. See the LICENSE file
     for details.
 """
 
@@ -16,7 +16,7 @@ import click
 def convert_num(given_num: int, convert_to: str) -> str:
     """
     Takes in a base 10 interger, uses a flag, to convert to
-        match which conversion is correct. 
+        match which conversion is correct.
 
     Args:
         given_num (int): decimal base 10 number
@@ -29,17 +29,16 @@ def convert_num(given_num: int, convert_to: str) -> str:
         ValueError if convert_to does not match, 'b', 'o', 'd', 'X'
     """
     match convert_to:
-        case 'b':
-            return format(given_num, 'b')
-        case 'o':
-            return format(given_num, 'o')
-        case 'd':
+        case "b":
+            return format(given_num, "b")
+        case "o":
+            return format(given_num, "o")
+        case "d":
             return str(given_num)
-        case 'X':
-            return format(given_num, 'X')
+        case "X":
+            return format(given_num, "X")
         case _:
             raise ValueError("No such base conversion type")
-        
 
 
 def input_validation(user_input: str) -> dict:
@@ -53,17 +52,17 @@ def input_validation(user_input: str) -> dict:
             boolean validations
     """
     return {
-        'b' : all(char in '01' for char in user_input),
-        'o' : all(char in '01234567' for char in user_input),
-        'd': user_input.isdecimal(),
-        'x': all(char in '0123456789ABCDEF' for char in user_input)
+        "b": all(char in "01" for char in user_input),
+        "o": all(char in "01234567" for char in user_input),
+        "d": user_input.isdecimal(),
+        "x": all(char in "0123456789ABCDEF" for char in user_input),
     }
 
 
 def input_to_int(input_num: str, input_base: str) -> int:
     """
     Takes a given input number as well as its flag for its base type, either
-    'b' for binary, 'o' for octal, 'd' for decimal, 'x' for hexidecimal. 
+    'b' for binary, 'o' for octal, 'd' for decimal, 'x' for hexidecimal.
     Performs input validation on the string.
 
     Args:
@@ -79,10 +78,10 @@ def input_to_int(input_num: str, input_base: str) -> int:
 
     # Enforcing user choice from click.Choice() ensures these will be the values
     base_conversion_functions = {
-    'b': lambda num: int(num, base=2),
-    'o': lambda num: int(num, base=8),
-    'd': int,
-    'x': lambda num: int(num, base=16),
+        "b": lambda num: int(num, base=2),
+        "o": lambda num: int(num, base=8),
+        "d": int,
+        "x": lambda num: int(num, base=16),
     }
 
     if not input_validation(input_num).get(input_base):
@@ -91,32 +90,39 @@ def input_to_int(input_num: str, input_base: str) -> int:
         return base_conversion_functions[input_base](input_num)
     except KeyError:
         raise ValueError("Invalid base type flag, only 'b', 'o', 'd' or 'x'")
-        
 
-@click.command()
-@click.argument('input_num')
-@click.argument('input_base', type=click.Choice(['b', 'o', 'd', 'x'], 
-                                                case_sensitive=False))
-@click.option('-b', '--binary', 'convert_to', flag_value='b', 
-            help='Convert to binary')
-@click.option('-o', '--octal', 'convert_to', flag_value='o', 
-            help='Convert to octal')
-@click.option('-d', '--decimal', 'convert_to', flag_value='d', 
-            help='Convert to decimal')
-@click.option('-x', '--hex', 'convert_to', flag_value='X', 
-            help='Convert to hexadecimal')
-def pyconv(input_num: str, input_base: str, convert_to: str) -> None:
+
+@click.group()
+def pyconv():
+    pass
+
+
+@pyconv.command()
+@click.argument("input_num")
+@click.argument(
+    "input_base", type=click.Choice(["b", "o", "d", "x"], case_sensitive=False)
+)
+@click.option("-b", "--binary", "convert_to", flag_value="b", help="Convert to binary")
+@click.option("-o", "--octal", "convert_to", flag_value="o", help="Convert to octal")
+@click.option(
+    "-d", "--decimal", "convert_to", flag_value="d", help="Convert to decimal"
+)
+@click.option(
+    "-x", "--hex", "convert_to", flag_value="X", help="Convert to hexadecimal"
+)
+def conv(input_num: str, input_base: str, convert_to: str) -> None:
     """
     Convert a number from one base to another.
-    
+
     Args:
         input_num (str): Number to convert.
         input_base (str): Base of the input number ('b', 'o', 'd', 'x').
         convert_to (str): Base to convert the number to ('b', 'o', 'd', 'X').
     """
     if not convert_to:
-        raise click.UsageError("You must specify a target conversion "\
-                            "base using -b, -o, -d, or -x.")
+        raise click.UsageError(
+            "You must specify a target conversion " "base using -b, -o, -d, or -x."
+        )
 
     try:
         click.echo(convert_num(input_to_int(input_num, input_base), convert_to))
@@ -126,6 +132,13 @@ def pyconv(input_num: str, input_base: str, convert_to: str) -> None:
         click.echo(ke)
 
 
-if __name__ == '__main__':
-    pyconv()    
+@pyconv.command()
+def from_file():
+    pass
 
+
+if __name__ == "__main__":
+    pyconv()
+
+# TODO investigate how convert command can be run by calling pyconv args
+# TODO make the from file run as a background process as not to lock the terminal
--- tests/test_pyconv.py
+++ tests/test_pyconv.py
@@ -2,26 +2,24 @@
 Filename: pyconv.py
 Author: Simon Crampton
 Created: May 16, 2024
-Description: This module functions along with click setup to quickly convert 
+Description: This module functions along with click setup to quickly convert
             numbers to different base types
 
 License:
-    This code is provided under the GPL V3 License. See the LICENSE file 
+    This code is provided under the GPL V3 License. See the LICENSE file
     for details.
 """
+
 from random import randint
 
 import pytest
-from pyconv.pyconv import (
-    convert_num, 
-    input_to_int,
-    input_validation)
+from pyconv.pyconv import convert_num, input_to_int, input_validation
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def base_nums() -> list:
     """
-        Fixture: Creates a random interger in range 1-50 returns a Tuple of 
+        Fixture: Creates a random interger in range 1-50 returns a Tuple of
         a single number of each popular base.
 
     Returns:
@@ -29,9 +27,9 @@ def base_nums() -> list:
         only one number of each.
     """
     dec_num: int = randint(1, 50)
-    bin_num: str = format(dec_num, 'b')
-    oct_num: str = format(dec_num, 'o')
-    hex_num: str = format(dec_num, 'X')
+    bin_num: str = format(dec_num, "b")
+    oct_num: str = format(dec_num, "o")
+    hex_num: str = format(dec_num, "X")
 
     return [bin_num, oct_num, str(dec_num), hex_num]
 
@@ -52,10 +50,10 @@ def number_perms(base_nums) -> dict:
     """
 
     return {
-        'b': base_nums[1:],
-        'o': [base_nums[0]] + base_nums[2:],
-        'd': base_nums[:2] + base_nums[3:],
-        'X': base_nums[:3]
+        "b": base_nums[1:],
+        "o": [base_nums[0]] + base_nums[2:],
+        "d": base_nums[:2] + base_nums[3:],
+        "X": base_nums[:3],
     }
 
 
@@ -71,16 +69,21 @@ def conversion_perms(base_nums) -> dict:
 
     Returns:
         dict: keys are single character flags matching the base type, 'b' for
-        binary etc. values are lists containing the other 3 base types after 
+        binary etc. values are lists containing the other 3 base types after
         calling the convert function.
     """
 
-    base_flags: list = ['b', 'o', 'd', 'X']
+    base_flags: list = ["b", "o", "d", "X"]
 
-    converted_nums: dict = {base: [convert_num(int(base_nums[2]), rem_base) for 
-                                rem_base in base_flags if rem_base != base]
-                                for base in base_flags}
-    
+    converted_nums: dict = {
+        base: [
+            convert_num(int(base_nums[2]), rem_base)
+            for rem_base in base_flags
+            if rem_base != base
+        ]
+        for base in base_flags
+    }
+
     return converted_nums
 
 
@@ -91,23 +94,27 @@ def test_if_conversions_are_accurate(number_perms, conversion_perms) -> None:
     as expected
 
     Args:
-        number_perms:keys are single character flags matching the base type, 
-        'b' for binary etc. values are lists containing the other 3 base 
+        number_perms:keys are single character flags matching the base type,
+        'b' for binary etc. values are lists containing the other 3 base
         type numbers.
         conversion_perms:keys are single character flags matching the base type,
-        'b' for binary etc. values are lists containing the other 3 base 
+        'b' for binary etc. values are lists containing the other 3 base
         types after calling the convert function.
     """
 
-    assert all(converted_val in conversion_perms['b'] for converted_val 
-            in number_perms['b'])
-    assert all(converted_val in conversion_perms['o'] for converted_val 
-            in number_perms['o'])
-    assert all(converted_val in conversion_perms['d'] for converted_val 
-            in number_perms['d'])
-    assert all(converted_val in conversion_perms['X'] for converted_val 
-            in number_perms['X'])
-    
+    assert all(
+        converted_val in conversion_perms["b"] for converted_val in number_perms["b"]
+    )
+    assert all(
+        converted_val in conversion_perms["o"] for converted_val in number_perms["o"]
+    )
+    assert all(
+        converted_val in conversion_perms["d"] for converted_val in number_perms["d"]
+    )
+    assert all(
+        converted_val in conversion_perms["X"] for converted_val in number_perms["X"]
+    )
+
 
 def test_convert_raises_value_error(base_nums) -> None:
     """
@@ -120,12 +127,12 @@ def test_convert_raises_value_error(base_nums) -> None:
     """
 
     with pytest.raises(ValueError):
-        convert_num(int(base_nums[2]), 'k')
+        convert_num(int(base_nums[2]), "k")
 
 
 def test_for_no_exception_for_correct_conversion_match(base_nums) -> None:
     """
-    Tests that the method convert_num does not raise a value error for 
+    Tests that the method convert_num does not raise a value error for
     expected convert flags.
 
     Args:
@@ -134,61 +141,65 @@ def test_for_no_exception_for_correct_conversion_match(base_nums) -> None:
     """
 
     with pytest.raises(ValueError, match=r""):
-        convert = convert_num(base_nums[2], 'b')
-        convert = convert_num(base_nums[2], 'o')
-        convert = convert_num(base_nums[2], 'd')
-        convert = convert_num(base_nums[2], 'X')
+        convert = convert_num(base_nums[2], "b")
+        convert = convert_num(base_nums[2], "o")
+        convert = convert_num(base_nums[2], "d")
+        convert = convert_num(base_nums[2], "X")
+
+        del convert
 
 
 def test_binary_validation() -> None:
     """
-    Tests validation function if an invalid binary (contains characters other 
+    Tests validation function if an invalid binary (contains characters other
     than 1's and 0's) is rejected and a valid binary is accepted
     """
-    invalid_binary: str = '10001113'
-    vaild_binary: str = '10111011'
+    invalid_binary: str = "10001113"
+    vaild_binary: str = "10111011"
 
-    assert input_validation(invalid_binary).get('b') is False
-    assert input_validation(vaild_binary).get('b') is True
+    assert input_validation(invalid_binary).get("b") is False
+    assert input_validation(vaild_binary).get("b") is True
+
 
 def test_oct_validation() -> None:
     """
     Tests both valid and invalid octal (contains an 8) to see if they are
     accepted or rejected respectively.
     """
-    invalid_oct_8: str = '1568'
-    invalid_oct_char: str = '133D'
-    valid_oct: str = '324'
+    invalid_oct_8: str = "1568"
+    invalid_oct_char: str = "133D"
+    valid_oct: str = "324"
 
-    assert input_validation(invalid_oct_8).get('o') is False
-    assert input_validation(invalid_oct_char).get('o') is False
-    assert input_validation(valid_oct).get('o') is True
+    assert input_validation(invalid_oct_8).get("o") is False
+    assert input_validation(invalid_oct_char).get("o") is False
+    assert input_validation(valid_oct).get("o") is True
+
 
 def test_dec_validation() -> None:
     """
-    Tests both valid and invalid decimnal(contains alphabetic values) to see 
+    Tests both valid and invalid decimnal(contains alphabetic values) to see
     if they are accepted or rejected respectively.
     """
-    invalid_dec: str = '123456f'
-    valid_dec: str = '1234567'
+    invalid_dec: str = "123456f"
+    valid_dec: str = "1234567"
 
-    assert input_validation(invalid_dec).get('d') is False
-    assert input_validation(valid_dec).get('d') is True
+    assert input_validation(invalid_dec).get("d") is False
+    assert input_validation(valid_dec).get("d") is True
 
 
 def test_hex_validation() -> None:
     """
-    Test both valid and invalid hexidecimal values(contains characters other 
-    than A-F or improper form 2a instead of 2A) to see if they are accepted 
+    Test both valid and invalid hexidecimal values(contains characters other
+    than A-F or improper form 2a instead of 2A) to see if they are accepted
     or rejected respectively.
     """
-    invalid_hex: str = '4H'
-    invalid_form_hex: str = '2a'
-    valid_hex: str = '4B'
+    invalid_hex: str = "4H"
+    invalid_form_hex: str = "2a"
+    valid_hex: str = "4B"
 
-    assert input_validation(invalid_hex).get('x') is False
-    assert input_validation(invalid_form_hex).get('x') is False
-    assert input_validation(valid_hex).get('x') is True
+    assert input_validation(invalid_hex).get("x") is False
+    assert input_validation(invalid_form_hex).get("x") is False
+    assert input_validation(valid_hex).get("x") is True
 
 
 def test_if_input_to_int_is_accurate(base_nums) -> None:
@@ -201,10 +212,10 @@ def test_if_input_to_int_is_accurate(base_nums) -> None:
         octal, decimal, and hex number converted from the same decimal number.
     """
 
-    assert input_to_int(base_nums[0], 'b') == int(base_nums[0], base=2)
-    assert input_to_int(base_nums[1], 'o') == int(base_nums[1], base=8)
-    assert input_to_int(base_nums[2], 'd') == int(base_nums[2])
-    assert input_to_int(base_nums[3], 'x') == int(base_nums[3], base=16)
+    assert input_to_int(base_nums[0], "b") == int(base_nums[0], base=2)
+    assert input_to_int(base_nums[1], "o") == int(base_nums[1], base=8)
+    assert input_to_int(base_nums[2], "d") == int(base_nums[2])
+    assert input_to_int(base_nums[3], "x") == int(base_nums[3], base=16)
 
 
 def test_if_input_to_int_raises_value_error() -> None:
@@ -212,8 +223,13 @@ def test_if_input_to_int_raises_value_error() -> None:
     Given invalid binary, octal, decimal, hexidecimal and base type flag
     """
     with pytest.raises(ValueError):
-        input_int = input_to_int('1002', 'b')
-        input_int = input_to_int('58', 'o')
-        input_int = input_to_int('23R', 'd')
-        input_int = input_to_int('5T', 'x')
-        input_int = input_to_int('12', 'h')
+        input_int = input_to_int("1002", "b")
+        input_int = input_to_int("58", "o")
+        input_int = input_to_int("23R", "d")
+        input_int = input_to_int("5T", "x")
+        input_int = input_to_int("12", "h")
+
+        del input_int
+
+
+# TODO run click integration tests
