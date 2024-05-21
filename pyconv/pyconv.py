7c11a4d9d6f8b2409d3befeefbf23a59a8c43cdc
DragonSGC <simongc100@gmail.com>
2024-05-21 17:49:30 +0200

    Added click command for file conversion

--- pyconv/pyconv.py
+++ pyconv/pyconv.py
@@ -10,7 +10,16 @@ License:
     for details.
 """
 
+import logging
 import click
+
+# Configure logging
+log_filename: str = "pyconv_errors.log"
+logging.basicConfig(
+    level=logging.ERROR,
+    format="%(asctime)s %(levelname)s %(message)s",
+    handlers=[logging.FileHandler(log_filename, mode="w")],
+)
 
 
 def convert_num(given_num: int, convert_to: str) -> str:
@@ -107,14 +116,14 @@ def convert_from_file_to_file(
         input_base (str): flag representing base type of numbers in the file.
     """
     with open(input_path, "r") as inp, open(output_path, "w") as out:
-        for line in inp:
+        for line_num, line in enumerate(inp, start=1):
             num: str = line.rstrip()
             try:
                 converted_num: str = convert_num(
                     input_to_int(num, input_base), convert_to
                 )
             except ValueError as ve:
-                print(ve)  # TODO add logging here as not to pollute terminal
+                logging.error("line: %s : %s", line_num, ve)
             else:
                 out.write(converted_num + "\n")
 
@@ -144,7 +153,7 @@ def conv(input_num: str, input_base: str, convert_to: str) -> None:
     Args:
         input_num (str): Number to convert.
         input_base (str): Base of the input number ('b', 'o', 'd', 'x').
-        convert_to (str): Base to convert the number to ('b', 'o', 'd', 'X').
+        convert_to (str): Base to convert the number to ('-b', '-o', '-d', '-x').
     """
     if not convert_to:
         raise click.UsageError(
@@ -160,8 +169,42 @@ def conv(input_num: str, input_base: str, convert_to: str) -> None:
 
 
 @pyconv.command()
-def from_file():
-    pass
+@click.argument(
+    "input_file", type=click.Path(exists=True, dir_okay=False, file_okay=True)
+)
+@click.argument("output_file", type=click.Path(exists=False))
+@click.argument(
+    "input_base", type=click.Choice(["b", "o", "d", "x"], case_sensitive=False)
+)
+@click.option("-b", "--binary", "convert_to", flag_value="b", help="Convert to binary")
+@click.option("-o", "--octal", "convert_to", flag_value="o", help="Convert to octal")
+@click.option(
+    "-d", "--decimal", "convert_to", flag_value="d", help="Convert to decimal"
+)
+@click.option(
+    "-x", "--hex", "convert_to", flag_value="X", help="Convert to hexadecimal"
+)
+def from_file(
+    input_file: click.Path, output_file: click.Path, input_base: str, convert_to: str
+) -> None:
+    """
+    Takes in command line arguments for a file containing numbers of one base
+    type, file must exist. An output file to which the converted numbers will
+    be written to, file will be created if it doesnt't exist.
+    Args:
+        input_file (click.Path): File path to file containing numbers of one base
+                                type to be converted
+        output_file (click.Path): File path to the file which will contain the
+                                converted numbers
+        input_base (str): Base of the input number ('b', 'o', 'd', 'x').
+        convert_to (str): Base to convert the number to ('-b', '-o', '-d', '-x').
+    """
+    if not convert_to:
+        raise click.UsageError(
+            "You must specify a target conversion " "base using -b, -o, -d, or -x."
+        )
+
+    convert_from_file_to_file(input_file, output_file, input_base, convert_to)
 
 
 if __name__ == "__main__":
@@ -169,4 +212,5 @@ if __name__ == "__main__":
 
 # TODO investigate how convert command can be run by calling pyconv args
 # TODO make the from file run as a background process as not to lock the terminal
-# TODO write to log file when exception not to pollute the terminal
+# TODO move file read and conversion logic to seperate script so it maybe called
+# in the backgoruond
